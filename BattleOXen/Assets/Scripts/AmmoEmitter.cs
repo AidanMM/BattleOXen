585994e2fdf2e1b669e281370428d8f13ff567b4
AidanMM <aidan@mcinerny.org>
2015-03-12 10:17:07 -0400

    Made sure to actually add the scene in this commit

---
+++ BattleOXen/Assets/Scripts/AmmoEmitter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmmoEmitter : MonoBehaviour {
+
+
+	public GameObject particle;
+	public Vector2 dir;
+	public int mag;
+	public int frequency;
+	public int max = 10;
+	private int count = 0;
+	private int timer = 0;
+
+	// Use this for initialization
+	void Start () {
+		dir *= mag;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
