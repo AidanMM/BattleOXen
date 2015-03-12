44b3144859149d76df8546e5edf218169ca03752
Tal Cohen <tcohen123@gmail.com>
2015-03-12 13:11:01 -0400

    Added some files from refactoring that I forgot.

---
+++ BattleOXen/Assets/Scripts/AmmoManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmmoManager : MonoBehaviour {
+	public bool shooting { get; set; }
+
+	// Use this for initialization
+	void Start () {
+		shooting = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ BattleOXen/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+	public GameObject PlayerPrefab;
+	public GameObject DispenserPrefab;
+	public int numPlayers;
+
+	//private List<GameObject> players;
+
+	// Use this for initialization
+	void Start () {
+		LimitNumPlayers ();
+		CreatePlayers ();
+		CreateDispenser ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void LimitNumPlayers() {
+		if (numPlayers > 3) {
+			numPlayers = 3;
+		} else if (numPlayers < 1) {
+			numPlayers = 1;
+		}
+	}
+
+	void CreatePlayers() {
+		GameObject player;
+		for (int i = 0; i < numPlayers; i++) {
+			player = (GameObject)Instantiate (PlayerPrefab, new Vector2 ((i+1)*50, 10), Quaternion.identity);
+			player.name = i.ToString();
+		}
+	}
+
+	void CreateDispenser() {
+		Instantiate (DispenserPrefab, new Vector2 (-1, 20), Quaternion.identity);
+	}
+}
