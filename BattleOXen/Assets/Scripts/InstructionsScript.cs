77afd25a2569c8c7d6e3ad92b9debca94d8b3220
AidanMM <aidan@mcinerny.org>
2015-05-02 06:19:46 -0400

    Made the instructions screen work.  And it is awesome!

--- BattleOXen/Assets/Scripts/InstructionsScript.cs
+++ BattleOXen/Assets/Scripts/InstructionsScript.cs
@@ -9,6 +9,7 @@ public class InstructionsScript : MonoBehaviour {
     string joystickAxis;
     string joystickBack;
     float oldAxis;
+    float axis;
 
 	// Use this for initialization
 	void Start () {
@@ -22,21 +23,19 @@ public class InstructionsScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
+        axis = Input.GetAxis(joystickAxis);
         if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.Space) ||
-            Input.GetButtonDown(joystickButton) || (oldAxis < .5f && Input.GetAxis(joystickAxis) > .5f) )
+            Input.GetButtonDown(joystickButton) || (oldAxis < .5f && axis > .5f) )
         {
             currentFrame++;
-            if (currentFrame > instructionFrames.Length)
+            if (currentFrame > instructionFrames.Length - 1)
             {
                 Application.LoadLevel("ModeSelect");
             }
             gameObject.GetComponent<SpriteRenderer>().sprite = instructionFrames[currentFrame];
         }
-        oldAxis = Input.GetAxis(joystickAxis);
-
         if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.Backspace) ||
-            Input.GetButtonDown(joystickBack) || (oldAxis > -0.5f && Input.GetAxis(joystickAxis) < -.5f))
+            Input.GetButtonDown(joystickBack) || ( axis < -.5f && oldAxis > -.5f ))
         {
             currentFrame--;
             if (currentFrame < 0)
@@ -45,7 +44,8 @@ public class InstructionsScript : MonoBehaviour {
             }
             gameObject.GetComponent<SpriteRenderer>().sprite = instructionFrames[currentFrame];
         }
-        oldAxis = Input.GetAxis(joystickAxis);
+        print(oldAxis + "   :   " +  axis);
+        oldAxis = axis;
 	
 	}
 }
